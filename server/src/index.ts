7f60ae887469d766efe6bf9e225fe1a25839cdb2
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-11 21:08:58 -0700

    Set up typeorm and express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World")
+import 'reflect-metadata'
+import express from 'express';
+
+import { PORT } from './utils/constants';
+import dataSource from './dataSource';
+
+const startServer = async () => {
+  await dataSource.initialize();
+  const app = express();
+
+  app.listen(PORT, () => {
+    console.log(`server started on localhost:${PORT}`);
+  });
+};
+
+startServer().catch((e) => {
+  console.error(e);
+})
