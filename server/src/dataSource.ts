85273b9585803299b47bd288d96dc613e386fdab
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-12 18:20:12 -0700

    Create user entity (#2)
    * Install dependencies for migrations and testing
    
    * Add script for migrations
    
    * Install dependencies for session persistance
    
    * Create entity for user
    
    * Create migrations

--- server/src/dataSource.ts
+++ server/src/dataSource.ts
@@ -3,12 +3,16 @@ import {
   DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME,
 } from './config/constants';
 
+import User from './entities/User';
+
 const dataSource = new DataSource({
   type: 'postgres',
   host: DB_HOST,
   port: parseInt(DB_PORT as string, 10),
   username: DB_USERNAME,
   password: DB_PASSWORD,
+  entities: [User],
+  migrations: ['src/migrations/*.ts'],
 });
 
 export default dataSource;
