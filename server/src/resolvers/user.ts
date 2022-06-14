0e8d3e01df14c3c904748bf5d87c7635b6e7d11b
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-13 19:53:01 -0700

    Create createUser resolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,11 +1,31 @@
-import { Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Field, InputType, Mutation, Query, Resolver,
+} from 'type-graphql';
 import User from '../entities/User';
 
+@InputType()
+class UsernamePasswordInput {
+  @Field(() => String)
+    username!: string;
+
+  @Field(() => String)
+    password!: string;
+}
 @Resolver()
 class UserResolver {
   @Query(() => [User])
   async users(): Promise<User[]> {
     return User.find();
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('input', () => UsernamePasswordInput) input: UsernamePasswordInput,
+  ) : Promise<User> {
+    return User.create({
+      ...input,
+    }).save();
   }
 }
 
