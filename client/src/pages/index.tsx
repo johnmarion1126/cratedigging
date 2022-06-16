f3b13dc9e6bd761e197658265cf23c13879e3d26
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-16 00:02:10 -0700

    Set up frontend (#6)
    * Create next app
    
    * Remove extra files
    
    * Install other dependencies
    
    * Create src folder
    
    * Set up chakra-ui

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import { Heading } from '@chakra-ui/react';
+
+const Home: NextPage = () => (
+  <Heading>
+    Hello World
+  </Heading>
+);
+
+export default Home;
