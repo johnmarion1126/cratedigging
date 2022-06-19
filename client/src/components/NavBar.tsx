63c3edc6103f0f9fc5250f80ded6d5112f5fc711
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-18 21:29:58 -0700

    Update nav if user is logged in

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,21 +1,58 @@
 import React from 'react';
 // eslint-disable-next-line import/no-named-default
 import { default as NextLink } from 'next/link';
-import { Box, Flex, Link } from '@chakra-ui/react';
+import {
+  Box, Button, Flex, Heading, Link,
+} from '@chakra-ui/react';
+import { useQuery } from '@apollo/client';
+
+import GET_USER_BY_QID from '../graphql/queries/getUserByQid';
 
 interface NavBarProps {}
 
-const NavBar: React.FC<NavBarProps> = () => (
-  <Flex bg="lightblue" p={4}>
-    <Box ml="auto">
-      <NextLink href="/login">
-        <Link mr={2} href="/login">Login</Link>
+const NavBar: React.FC<NavBarProps> = () => {
+  const { data, loading } = useQuery(GET_USER_BY_QID);
+
+  let body = null;
+
+  if (loading) {
+    body = null;
+  } else if (!data?.getUserByQid) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2} href="/login">Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link mr={2} href="/register">Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>
+          {data.getUserByQid.username}
+        </Box>
+        <Button
+          variant="link"
+        >
+          Log Out
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex zIndex={2} position="sticky" top={0} bg="lightblue" p={4} align="center">
+      <NextLink href="/">
+        <Heading>cratedigging</Heading>
       </NextLink>
-      <NextLink href="/register">
-        <Link mr={2} href="/register">Register</Link>
-      </NextLink>
-    </Box>
-  </Flex>
-);
+      <Box ml="auto">
+        {body}
+      </Box>
+    </Flex>
+  );
+};
 
 export default NavBar;
