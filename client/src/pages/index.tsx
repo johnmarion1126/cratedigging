fb87eae213a63e110e3eb6afef6831a6e1a40b58
John Marion Sanchez <johnmarion1126@gmail.com>
2022-06-25 17:09:26 -0700

    Create a link to create-post page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import { Heading } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+// eslint-disable-next-line import/no-named-default
+import { default as NextLink } from 'next/link';
 
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => (
   <Layout>
-    <Heading>
-      Hello World
-    </Heading>
+    <Flex mb={4}>
+      <NextLink href="/create-post">
+        <Button ml="auto">
+          Create Post
+        </Button>
+      </NextLink>
+    </Flex>
   </Layout>
 );
 
